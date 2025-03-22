801ef7698f86cbe4ee765a094f472d9beaa9f659
Osvaldo Alves <oalves.neto@outlook.com>
2025-03-22 09:58:52 -0300

    feat: add variables example

---
+++ @1-variables/01-variables.js
@@ -0,0 +1,18 @@
+/**
+ * Fundamentos de javascript
+ * 
+ * @author Osvaldo A.
+ * @date 22/03/2025
+ * @description Diferenças entre variáveis em javascript
+ */
+
+/** let: multaveis, ou seja o seu valor pode ser alterado durante a execução do programa */
+let customer = "João"
+console.log("Nome do cliente:", customer)
+
+customer = "Pedro"
+console.log("O nome do cliente mudou:", customer)
+
+/** const: imultaveis, uma vez atribuído o valor ela não pode ser alterada */
+const coffe = "Expresso"
+console.log("Pedido:", coffe)
